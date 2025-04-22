293b8014f95a6bc6416a8eb92fa3913c08403146
haitham <haithamsm95@gmail.com>
2025-04-22 08:45:21 +0200

    event handling functions, onCancel and the button toggling the visiablitiy of the input, better DX

--- Refreshing-react/src/components/CustomerTable/CustomerTable.jsx
+++ Refreshing-react/src/components/CustomerTable/CustomerTable.jsx
@@ -5,12 +5,20 @@ import NewEntryInput from "../NewEntryInput/NewEntryInput";
 function CustomerTable({ customers, onSelectCustomer, selectedCustomerId }) {
   const [isAddNewCustomer, setIsAddNewCustomer] = useState(false);
 
+  const handleCancel = (e) => {
+    setIsAddNewCustomer(false);
+  };
+
+  const handleButtonToggle = (e) => {
+    setIsAddNewCustomer(!isAddNewCustomer);
+  };
+
   return (
     <div className="flex flex-col basis-1/2">
       <div className="flex justify-between items-center border-b pb-2 px-4 py-2">
         <h2 className="text-2xl">Customers</h2>
         <button
-          onClick={() => setIsAddNewCustomer(!isAddNewCustomer)}
+          onClick={handleButtonToggle}
           className="py-1 px-5 bg-amber-200 rounded font-bold "
         >
           Add new
